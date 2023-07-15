74fcccce26a1be7b60e08ebcd8df47ab70248ed6
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-07-16 00:20:17 +0700

    add function getUser & deleteUser|controller_user

--- lib/controller/controller_user.dart
+++ lib/controller/controller_user.dart
@@ -1,6 +1,23 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ControllerUser {
   final userColection = FirebaseFirestore.instance.collection('Users');
-  
+  final auth = FirebaseAuth.instance;
+  final StreamController<List<DocumentSnapshot>> streamController =
+      StreamController<List<DocumentSnapshot>>.broadcast();
+  Stream<List<DocumentSnapshot>> get stream => streamController.stream;
+
+  Future getUser() async {
+    final user = await userColection.get();
+    streamController.sink.add(user.docs);
+    return user.docs;
+  }
+
+  Future deleteUser(String id) async {
+    final user = await userColection.doc(id).delete();
+    return user;
+  }
 }
