69c03c23b3036fe3706b1bc72b183e6f5d073c58
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-07-15 21:43:42 +0700

    add model_user.dart sebagai model dasar untuk user

---
+++ lib/model/model_user.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+class ModelUser {
+  String? id;
+  String name;
+  String email;
+  String role;
+  String is_admin;
+  String phone;
+  ModelUser({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.role,
+    required this.is_admin,
+    required this.phone,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'role': role,
+      'is_admin': is_admin,
+      'phone': phone,
+    };
+  }
+
+  factory ModelUser.fromMap(Map<String, dynamic> map) {
+    return ModelUser(
+      id: map['id'],
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      role: map['role'] ?? '',
+      is_admin: map['is_admin'] ?? '',
+      phone: map['phone'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ModelUser.fromJson(String source) => ModelUser.fromMap(json.decode(source));
+}
